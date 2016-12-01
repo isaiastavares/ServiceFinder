87166afb29b38f19fb1f4b65f0a9248599b95468
Isaias Tavares <isaiasengsoft@gmail.com>
2016-12-01 18:53:41 -0200

    Criando tela de Detalhes de Serviço

---
+++ ServiceFinder/ServiceFinder/DetalhesServicoViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetalhesServicoViewController.swift
+//  ServiceFinder
+//
+//  Created by Student on 12/1/16.
+//  Copyright © 2016 ServiceFinder. All rights reserved.
+//
+
+import UIKit
+
+class DetalhesServicoViewController: UIViewController {
+
+    var servico : Servico?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if servico != nil {
+        }
+    }
+
+}
