8690e2a5c407556ab808f811082506b08f678686
Igor Brandao <igor-brandao@outlook.com>
2016-12-01 19:14:01 -0200

    Implementando comparable em data

--- ServiceFinder/ServiceFinder/Data.swift
+++ ServiceFinder/ServiceFinder/Data.swift
@@ -8,16 +8,20 @@
 
 import Foundation
 
-class Data {
+class Data : Comparable{
 
     var dia : Int
     var mes : Int
     var ano : Int
+    var hora: Int
+    var minuto: Int
     
-    init(dia: Int, mes: Int, ano: Int) {
+    init(dia: Int, mes: Int, ano: Int, hora: Int, minuto: Int) {
         self.dia = dia
         self.mes = mes
         self.ano = ano
+        self.hora = hora
+        self.minuto = minuto
     }
     
     func getDia() -> Int {
@@ -32,4 +36,30 @@ class Data {
         return self.ano
     }
     
+    func getHora() -> Int {
+        return self.hora
+    }
+    
+    func getMinuto() -> Int {
+        return self.minuto
+    }
+    
+}
+
+func < (lhs: Data, rhs: Data) -> Bool {
+    return
+        lhs.getAno() > rhs.getAno() ? false :
+            lhs.getMes() > rhs.getMes() ? false :
+                lhs.getDia() > rhs.getDia() ? false :
+                    lhs.getHora() > rhs.getHora() ? false :
+                        lhs.getMinuto() > rhs.getMinuto() ? false : true
+}
+
+func == (lhs: Data, rhs: Data) -> Bool {
+    return
+        lhs.getAno() != rhs.getAno() ? false :
+            lhs.getMes() != rhs.getMes() ? false :
+                lhs.getDia() != rhs.getDia() ? false :
+                    lhs.getHora() != rhs.getHora() ? false :
+                        lhs.getMinuto() != rhs.getMinuto() ? false : true
 }
