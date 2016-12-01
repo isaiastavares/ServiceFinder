bb5c96475f3c33c723d251d8acb009099a53567d
Igor Brandao <igor-brandao@outlook.com>
2016-12-01 20:57:15 -0200

    Melhorando mocks de serviço

--- ServiceFinder/ServiceFinder/Servico.swift
+++ ServiceFinder/ServiceFinder/Servico.swift
@@ -29,10 +29,10 @@ class Servico {
         
         let usuarios = Usuario.getLista()
         
-        let servico1 = Servico(usuario: usuarios[0], categoria: "TI", descricao: "Formato PC, notebook, limpo virus, faço backup e manutenções em aparelhos", avaliacao: 4.5, valor: 180.00)
-        let servico2 = Servico(usuario: usuarios[0], categoria: "Pedreiro", descricao: "Faço casas, paredes, robocos, azulejos e reformas em geral", avaliacao: 4.8, valor: 1000.00)
-        let servico3 = Servico(usuario: usuarios[1], categoria: "Diarista", descricao: "Formato PC", avaliacao: 3.2, valor: 150.00)
-        let servico4 = Servico(usuario: usuarios[2], categoria: "Encanador", descricao: "Faço casas", avaliacao: 2.3, valor: 600.00)
+        let servico1 = Servico(usuario: usuarios[0], categoria: "TI", descricao: "Formato PC, notebook, limpo virus, faço backup e manutenções em aparelhos.", avaliacao: 4.5, valor: 180.00)
+        let servico2 = Servico(usuario: usuarios[0], categoria: "Pedreiro", descricao: "Faço casas, paredes, robocos, azulejos e reformas em geral.", avaliacao: 4.8, valor: 1000.00)
+        let servico3 = Servico(usuario: usuarios[1], categoria: "Diarista", descricao: "Limpo casas, apartamentos, casebres e faço faxinas em empresas.", avaliacao: 3.2, valor: 150.00)
+        let servico4 = Servico(usuario: usuarios[2], categoria: "Encanador", descricao: "Desentupo pias, vasos, passo encanamentos e serviços variados envolvendo tubos e conexões", avaliacao: 2.3, valor: 600.00)
         
         return [
             servico1,
