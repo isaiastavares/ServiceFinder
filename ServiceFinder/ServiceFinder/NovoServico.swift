e05a4579d3b9259df3cd0683fb318c8d275f4cb3
Rafael <rafaelcdmm@gmail.com>
2016-12-01 21:08:10 -0200

    Adiciona Tela Novo Servico

---
+++ ServiceFinder/ServiceFinder/NovoServico.swift
@@ -0,0 +1,18 @@
+//
+//  NovoServico.swift
+//  ServiceFinder
+//
+//  Created by Student on 01/12/16.
+//  Copyright © 2016 ServiceFinder. All rights reserved.
+//
+
+import Cocoa
+
+class NovoServico: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
